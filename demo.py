ae28df7837e82a4d8dbc20cc2bc1e6fbec0b2a88
kimi <kimi307@hotmail.fr>
2021-06-19 23:05:14 +0200

    hello a

--- demo.py
+++ demo.py
@@ -1,4 +1,14 @@
-print("hello")
-a=4
-if a>1:
-    print("hello a")
+# This code is meant to provide efficient frontier for an asset universe selected from
+# JpMorgan Capital Market Assumption
+
+import pandas as pd
+import numpy as np
+
+xl_file = pd.read_excel("Input configuration.xlsx",sheet_name=None)
+
+sheets = xl_file.keys()
+
+inputconfig=xl_file['Input Config']
+listheader=list(inputconfig)
+print(listheader)
+print(inputconfig.loc[:,listheader[1]])
